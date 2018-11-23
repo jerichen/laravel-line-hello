4ec3e3fa19431337057a8cdc6cbee9622057c8e2
jerichen <jerichen@cw.com.tw>
2018-11-23 23:20:22 +0800

    add sdk

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Log;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 
 class LineController extends Controller
 {
