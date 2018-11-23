940f557e4e1379b87616b99c545c1afecff04749
jerichen <jerichen@cw.com.tw>
2018-11-23 18:23:46 +0800

    change route

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ Route::get('/', function () {
 
 
 // line-bot
-Route::get('line/reply', 'LineController@reply')->name('line.reply');
+Route::post('line/reply', 'LineController@reply')->name('line.reply');
 Route::get('line/push', 'LineController@push')->name('line.push');
