2c080cd971bca4e0737e3eb4cdf3fdacd13f27d3
jerichen <jerichen@cw.com.tw>
2018-11-23 23:31:55 +0800

    add predis

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 // line-bot
 Route::post('line/reply', 'LineController@reply')->name('line.reply');
 Route::get('line/push', 'LineController@push')->name('line.push');
